d81ee594b21ab001695249042ed088e40687beb8
Leandro Azevedo <leandrorazevedo@gmail.com>
2020-07-24 19:32:33 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core';
+import React from 'react';
+
+import Sistema from './pages/sistemas';
+
+const useStyles = makeStyles({
+  app: {
+    height: '100vh',
+    width: '100%',
+  }
+})
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <Sistema></Sistema>
     </div>
   );
 }
